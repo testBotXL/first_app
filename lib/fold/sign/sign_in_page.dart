db6a49f4879e2a4107b625e9746746a4b76089ff
testBotXL <aafahim98@gmail.com>
2022-05-23 11:34:53 +0600

    buttons layout

--- lib/fold/sign/sign_in_page.dart
+++ lib/fold/sign/sign_in_page.dart
@@ -8,7 +8,41 @@ class SignInPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Vehicle Tracker'),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Vehicle Tracker'))
+          ],
+        ),
+        elevation: 15.0,
+      ),
+      body: Container(
+        color: Colors.blueAccent,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              color: Colors.black,
+              child: SizedBox(
+                height: 75.0,
+              ),
+            ),
+            Container(
+              color: Colors.red,
+              child: SizedBox(
+                height: 75.0,
+              ),
+            ),
+            Container(
+              color: Colors.yellow,
+              child: SizedBox(
+                height: 75.0,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
