d611d186d42a38d01001d3b79dd62937a237666a
testBotXL <aafahim98@gmail.com>
2022-07-11 13:55:17 +0600

    Online test

---
+++ lib/fold/sign/online.dart
@@ -0,0 +1,92 @@
+import 'package:first_app/common_widgets/custom_raised_button.dart';
+import 'package:first_app/fold/sign/sign_in_button.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Online extends StatelessWidget {
+  const Online({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+      AppBar(
+
+
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Vehicle Tracker'), alignment: Alignment.center, )
+          ],
+        ),
+        elevation: 15.0,
+        leading: Image.asset('images/just.png'),
+      ),
+
+      body: _buildOnline(),
+      backgroundColor: Colors.grey[400],
+    );
+  }
+
+  Widget _buildOnline() {
+    return Padding(
+      padding: EdgeInsets.all(60.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          /*Text(
+            'Track Vehicle',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.w800,
+            ),
+          ),*/
+          SizedBox(height: 15.0),
+          SignInButton(
+            text: 'Online Vehicle',
+            textColor: Colors.black87,
+            color: Colors.blueAccent,
+
+            onPressed: () {
+
+            },
+
+          ),
+          SizedBox(height: 18.0),
+          SignInButton(
+            text: 'Offline Vehicle',
+            textColor: Colors.black87,
+            color: Colors.blueAccent,
+
+            onPressed: () {},
+
+          ),
+          SizedBox(height: 18.0),
+          SignInButton(
+            text: 'General Tracking',
+            textColor: Colors.black87,
+            color: Colors.blueAccent,
+
+            onPressed: () {},
+
+          ),
+          SizedBox(height: 18.0),
+          SignInButton(
+            text: 'Search Vehicle',
+            textColor: Colors.black87,
+            color: Colors.blueAccent,
+
+            onPressed: () {},
+
+          ),
+
+
+        ],
+      ),
+    );
+  }
+}
--- lib/fold/sign/sign_in_page.dart
+++ lib/fold/sign/sign_in_page.dart
@@ -3,79 +3,74 @@ import 'package:first_app/fold/sign/sign_in_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'online.dart';
+
 class SignInPage extends StatelessWidget {
   const SignInPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:
-       AppBar(
-
-
+      appBar: AppBar(
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Container(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('Vehicle Tracker'), alignment: Alignment.center, )
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Vehicle Tracker'),
+              alignment: Alignment.center,
+            )
           ],
         ),
         elevation: 15.0,
         leading: Image.asset('images/just.png'),
       ),
+      body:
 
-      body: _buildContent(),
-      backgroundColor: Colors.grey[400],
-      );
-  }
-
-  Widget _buildContent() {
-    return Padding(
-      padding: EdgeInsets.all(60.0),
-      child: Column(
+         Padding(
+        padding: EdgeInsets.all(60.0),
+        child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          /*Text(
-            'Track Vehicle',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 25.0,
-              fontWeight: FontWeight.w800,
-            ),
-          ),*/
-          SizedBox(height: 15.0),
-          SignInButton(
-            text: 'Track Vehicle',
-            textColor: Colors.black87,
-            color: Colors.blueAccent,
-
-            onPressed: () {},
-
-          ),
-          SizedBox(height: 18.0),
-          SignInButton(
-            text: 'Registration',
-            textColor: Colors.black87,
-            color: Colors.blueAccent,
-
-            onPressed: () {},
-
-          ),
-          SizedBox(height: 18.0),
-          SignInButton(
-            text: 'Reservation',
-            textColor: Colors.black87,
-            color: Colors.blueAccent,
-
-            onPressed: () {},
-
-          ),
-
-
+        /*Text(
+                'Track Vehicle',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),*/
+        SizedBox(height: 15.0),
+        SignInButton(
+        text: 'Track Vehicle',
+        textColor: Colors.black87,
+        color: Colors.blueAccent,
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => Online()),);
+        },
+        ),
+        SizedBox(height: 18.0),
+        SignInButton(
+        text: 'Registration',
+        textColor: Colors.black87,
+        color: Colors.blueAccent,
+        onPressed: () {},
+        ),
+        SizedBox(height: 18.0),
+        SignInButton(
+        text: 'Reservation',
+        textColor: Colors.black87,
+        color: Colors.blueAccent,
+        onPressed: () {},
+        ),
         ],
-      ),
+        ),
+        )
+
+
     );
   }
+
+
 }
